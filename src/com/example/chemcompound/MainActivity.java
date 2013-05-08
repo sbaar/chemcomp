0be0eae66ae5a7639e4640e9462da085b22e8546
Scott Baar <scottpbaar@gmail.com>
2013-05-08 13:47:11 -0700

    commit

--- src/com/example/chemcompound/MainActivity.java
+++ src/com/example/chemcompound/MainActivity.java
@@ -3,19 +3,25 @@ package com.example.chemcompound;
 import java.util.ArrayList;
 import java.util.List;
 
-//import org.openscience.cdk.Molecule;
 import org.openscience.cdk.interfaces.IAtomContainer;
-//import org.openscience.cdk.layout.StructureDiagramGenerator;
+import org.openscience.cdk.layout.StructureDiagramGenerator;
 import org.openscience.cdk.renderer.AtomContainerRenderer;
 import org.openscience.cdk.renderer.font.AWTFontManager;
 import org.openscience.cdk.renderer.generators.BasicAtomGenerator;
 import org.openscience.cdk.renderer.generators.BasicBondGenerator;
 import org.openscience.cdk.renderer.generators.BasicSceneGenerator;
-//import org.openscience.cdk.templates.MoleculeFactory;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+//import org.openscience.cdk.Molecule;
+//import org.openscience.cdk.layout.StructureDiagramGenerator;
+//import org.openscience.cdk.templates.MoleculeFactory;
 
 
 
@@ -35,13 +41,13 @@ public class MainActivity extends Activity {
 		  WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB
 		);*/
 
-		//IAtomContainer triazole = MoleculeFactory.make123Triazole();
-		///try{StructureDiagramGenerator sdg = new StructureDiagramGenerator();
-		//sdg.setMolecule((Molecule) triazole);
-	//	sdg.generateCoordinates();
-		//triazole = sdg.getMolecule();
-	//	}
-		//catch (Exception e) {}
+		IAtomContainer triazole = MoleculeFactory.make123Triazole();
+		try{StructureDiagramGenerator sdg = new StructureDiagramGenerator();
+		sdg.setMolecule((IAtomContainer) triazole);
+		sdg.generateCoordinates();
+		triazole = sdg.getMolecule();
+			}
+		catch (Exception e) {}
 
 		// generators make the image elements
 		List generators = new ArrayList();
@@ -74,7 +80,27 @@ public class MainActivity extends Activity {
 		
 		
 	}
+	private class DemoView extends View{
+		public DemoView(Context context){
+			super(context);
+		}
 
+		@Override protected void onDraw(Canvas canvas) {
+			super.onDraw(canvas);
+
+			// custom drawing code here
+			// remember: y increases from top to bottom
+			// x increases from left to right
+			int x = 0;
+			int y = 0;
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.FILL);
+
+			// make the entire canvas white
+			paint.setColor(Color.WHITE);
+			canvas.drawPaint(paint);
+		}
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
